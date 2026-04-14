1d32b75438708998157d216f611b35a90d2f778a
8812392 <2016189@ikns.edu.bh>
2026-04-14 18:04:08 +0300

    app.py

--- app.py
+++ app.py
@@ -17,8 +17,7 @@ from sklearn.linear_model import LogisticRegression
 # Page config
 # =========================
 st.set_page_config(
-    page_title="Bahrain Sustainable Fishing Classifier",
-    page_icon="🇧🇭",
+    page_title="SMART SEAS"
     layout="wide",
 )
 
